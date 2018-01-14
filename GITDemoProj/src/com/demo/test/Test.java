80a7a4dc05a34b469279fe0641a87471624464e8
its me <its me@Prashant>
2018-01-14 18:58:28 +0530

    tl

--- GITDemoProj/src/com/demo/test/Test.java
+++ GITDemoProj/src/com/demo/test/Test.java
@@ -2,7 +2,8 @@ package com.demo.test;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void 
+	main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test......");
 
