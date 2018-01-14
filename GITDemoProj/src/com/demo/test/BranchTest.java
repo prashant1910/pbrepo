7a286c36f184ee770ab60b436a9bebe8e9ae1970
its me <its me@Prashant>
2018-01-14 19:13:01 +0530

    dev

--- GITDemoProj/src/com/demo/test/BranchTest.java
+++ GITDemoProj/src/com/demo/test/BranchTest.java
@@ -5,7 +5,7 @@ public class BranchTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("New Branch........");
-
+		System.out.println("modified");
 	}
 
 }
